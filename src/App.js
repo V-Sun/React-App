fd00b9c54d0001bf80615103d10a7aee65e68385
V-Sun <varun.sunkaraneni@gmail.com>
2025-07-30 19:45:32 -0500

    Inital Upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 function App() {
+
+  const [name, setName] = useState("Varun");
+  const [age, setAge] = useState(20);
+  console.log("in APP Component")
+  const handleClick = () => {
+    setName("Akhil");
+    setAge(30);
+    console.log(age, name);
+  }
   return (
     <div className="App">
       <div className="content">
        <h2>My App</h2>  
-      </div> 
+       <p>{name} is {age} years old.</p>
+      </div>
+      <button onClick={handleClick}> Click Me! </button> 
     </div>
   );
 }
